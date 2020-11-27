7355acc1421d4aca786bb5a37f6a06ace7d3bd27
tanzeel <tanzeel.mirza@philips.com>
2020-11-27 23:30:07 +0530

    keeping minimal code

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT;
+const port = (process.env.PORT);
 const app = express();
 
-const api = require('./routes/api');
 const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api', api);
-
 app.get('/', function(req, res) {
     res.send('Hello from the server');
 })
 
-app.listen(PORT, function() {
+app.listen((process.env.PORT), function() {
     console.log('Server listening on PORT ' + port)
 });
