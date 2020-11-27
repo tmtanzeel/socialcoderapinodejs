77430dd8699e56c416e142d4262138f9fbf2c3f1
tanzeel <tanzeel.mirza@philips.com>
2020-11-27 22:26:33 +0530

    removed hard coded port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port;
 const app = express();
 
 const api = require('./routes/api');
