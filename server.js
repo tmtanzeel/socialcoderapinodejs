e5ecdd01cf4be26eec6e40b32fb85cc03f31ac2f
tanzeel <tanzeel.mirza@philips.com>
2020-12-12 21:27:19 +0530

    fixed cors

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/', function(req, res) {
     res.send('Server is up and running!');
 })
 
-app.listen((3000), function() {
+app.listen(((process.env.PORT)), function() {
     console.log('Server listening on 3000 port');
 });
