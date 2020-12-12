c6b31f7373c316015fe9cd2cde531d1ad0a85f4f
tanzeel <tanzeel.mirza@philips.com>
2020-12-12 19:37:58 +0530

    added more headers

--- server.js
+++ server.js
@@ -7,9 +7,25 @@ const api = require('./routes/api');
 const cors = require('cors');
 
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'https://firefly-camp.azurewebsites.net/'
-}));
+
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://firefly-camp.azurewebsites.net/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use('/api', api);
 app.get('/', function(req, res) {
