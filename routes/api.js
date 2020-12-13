8d8d232e1558e895cfc7661f92e40cab54bbe510
Tanzeel Mirza <tmtanzeel9@gmail.com>
2020-12-13 22:39:13 +0530

    removed passwprd

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Article = require('../models/article');
 const User = require('../models/user');
 const Post = require('../models/article');
-const db = 'mongodb://tanzeel_123:mydbpass@cluster0-shard-00-00-znt38.mongodb.net:27017,cluster0-shard-00-01-znt38.mongodb.net:27017,cluster0-shard-00-02-znt38.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+const db = 'mongodb://tanzeel_123:somepass@cluster0-shard-00-00-znt38.mongodb.net:27017,cluster0-shard-00-01-znt38.mongodb.net:27017,cluster0-shard-00-02-znt38.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 const jwt = require('jsonwebtoken');
 
 mongoose.connect(db, { useNewUrlParser: true, useFindAndModify: false }, err => {
